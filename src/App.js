70e3875e14670b5df0c63fcb581fb2e05132aac4
Aurelie <josserand.n.aurelie@gmail.com>
2024-02-26 11:42:22 -0500

    Creates and style Die components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./style.css";
+import Die from "./Die.js";
 
-function App() {
+export default function App() {
   return (
     <main>
-      <h1>Hello world</h1>
+      <div id="die-container">
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={7} />
+        <Die value={8} />
+        <Die value={9} />
+        <Die value={10} />
+      </div>
     </main>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -9,9 +9,3 @@ root.render(
   </React.StrictMode>
 );
 
-
-// Challenge: Start a brand new React app!
-// Create a separate App component
-// Import and render the App component here
-// In the App component, render a <main> element
-// Style everything to look like the slide
