7c8994e6689c9799ba05362de00c328b70d52712
Aurelie <josserand.n.aurelie@gmail.com>
2024-02-25 20:39:11 -0500

    add all files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello world</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+// Challenge: Start a brand new React app!
+// Create a separate App component
+// Import and render the App component here
+// In the App component, render a <main> element
+// Style everything to look like the slide
